2d6d0dd9a171be913a7ae9ee0e1c093058657264
Vedant Mandwe <vedantmandwe5@gmail.com>
2025-03-04 20:23:49 +0530

    Added env folder and files

---
+++ environments/dev.env.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoDbConnectionString: 'mongodb+srv://Admin:1zTkRy01najR8Z8f@dev-cluster.bgktn.mongodb.net/',
+    mailGunAPIKey : '8fd9a2fd002a816c9457ea1df4712598-667818f5-83f24c6e',
+    mailGunDomain : 'sandbox03271bbb8a1a445a91edc8df54ba2344.mailgun.org',
+    appPasswordKey : 'qvqg fxru ruur bkpf'
+  };
---
+++ environments/prod.env.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoDbConnectionString: '',
+        mailGunAPIKey : '8fd9a2fd002a816c9457ea1df4712598-667818f5-83f24c6e'
+  };
