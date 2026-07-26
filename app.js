0860300a9c14fe2f3f1fd98869e3c1df2816e2df
Vedant <vedantmandwe5@gmail.com>
2026-07-26 15:19:42 +0530

    refactor: separate Express app from server startup

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const incomeRoutes = require('./routes/incomeRoutes');
+const expenseRoutes = require('./routes/expenseRoutes');
+const dashboardRoutes = require('./routes/dashboardRoutes')
+
+const app = express();
+require('dotenv').config();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'], allowedHeaders: ['Content-Type', 'Authorization'] }));
+app.use(bodyParser.json());
+
+//Routes
+app.use('/auth', authRoutes);
+app.use('/income', incomeRoutes);
+app.use('/expense', expenseRoutes);
+app.use('/dashboard', dashboardRoutes);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,29 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const incomeRoutes = require('./routes/incomeRoutes');
-const expenseRoutes = require('./routes/expenseRoutes');
-const dashboardRoutes = require('./routes/dashboardRoutes')
-
-const app = express();
-require('dotenv').config();
-
+const app = require('./app');
 const PORT = process.env.PORT || 3000;
-
-// Connect to MongoDB
-connectDB();
-
-// Middleware
-app.use(cors({ origin: '*', methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'], allowedHeaders: ['Content-Type', 'Authorization'] }));
-app.use(bodyParser.json());
-
-//Routes
-app.use('/auth', authRoutes);
-app.use('/income', incomeRoutes);
-app.use('/expense', expenseRoutes);
-app.use('/dashboard', dashboardRoutes);
 
 //Start Server
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
